2df4dba0bf4d57f82844d372cd7f7a7069184f21
deneb1593 <deneb1593@gmail.com>
2023-01-17 01:07:24 +0900

    converter 설명 추가

--- src/main/java/io/study/deneb/security/RsaKeyProperties.java
+++ src/main/java/io/study/deneb/security/RsaKeyProperties.java
@@ -7,4 +7,7 @@ import java.security.interfaces.RSAPublicKey;
 
 @ConfigurationProperties(prefix = "rsa")
 public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
+  // 생성자 내 RSApublickey를 지정하면
+  // Spring genericServiceConverter가 properties내 pem 값을 읽어 RSAPublicKey로 변환하는
+  // RSAConverter를 호출한다.
 }
